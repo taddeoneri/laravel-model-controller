fa1835f3426786cf8ebfb7d24d40af3a7d079653
taddeoneri <taddeo.neri10@gmail.com>
2023-05-23 17:07:52 +0200

    fixed

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -3,17 +3,17 @@
 @section('content')
     <section class="container text-center">
         <h1 class="py-4">Lista dei migliori film</h1>
-        <div class="d-flex justify-content-between flex-wrap">
-            @foreach ($movies as $movie)
-            <div class="card my-5">
-                <img src="{{ $movie->image }}" class="card-img-top" alt="{{ $movie->title }}">
-                <div class="card-body">
-                  <h5 class="card-title">{{ $movie->title }}</h5>
-                  <p class="card-text">{{ $movie->date }}</p>
-                  <p class="card-text">{{ $movie->vote }}</p>
-                </div>
-              </div>
-        @endforeach
-        </div>
+            <div class="d-flex justify-content-between flex-wrap">
+                @foreach ($movies as $movie)
+                    <div class="card my-5">
+                        <img src="{{ $movie->image }}" class="card-img-top" alt="{{ $movie->title }}">
+                        <div class="card-body">
+                        <h5 class="card-title">{{ $movie->title }}</h5>
+                        <p class="card-text">{{ $movie->date }}</p>
+                        <p class="card-text">{{ $movie->vote }}</p>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
     </section>
 @endsection
