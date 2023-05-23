5dfe03d52f06a98a026c4c8520cd942fdabb7e69
taddeoneri <taddeo.neri10@gmail.com>
2023-05-23 16:34:08 +0200

    Homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="container">
-        <h1>Home page</h1>
-        <p>Hello world</p>
-        <a href="{{ route('books.index') }}">Vai alla lista dei libri</a>
-    </section>
+    <div class="container d-flex py-5 align-items-center flex-column">
+        <h1>Lista titoli che puoi vedere</h1>
+        <p>Premi per goderti tutti i titoli</p>
+        <a href="{{ route('movies.index') }}">Vai</a>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
